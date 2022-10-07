4184fb5c6a4c3b3aee2e5b336ee9097c76b5a067
Elton Lobo <eltonsw00@gmail.com>
2022-10-08 00:48:40 +0530

    Add basic product description page

--- src/components/DisplayMaxProductInfo.js
+++ src/components/DisplayMaxProductInfo.js
@@ -1,13 +1,16 @@
 import React from "react";
 import service from "../services";
 import UnknownPath from "./UnknownPath";
+import AppState from "../AppState";
 
-export default class SingleProduct extends React.Component {
+export default class DisplayMaxProductInfo extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            product: undefined
+            product: undefined,
+            imgIdx: 0
         };
+        this.descriptionRef = React.createRef();
     }
 
     componentDidMount() {
@@ -18,6 +21,11 @@ export default class SingleProduct extends React.Component {
                .catch(err => console.log(err.message));
     }
 
+    componentDidUpdate() {
+        if (this.state.product && this.descriptionRef.current.innerHTML === "")
+            this.descriptionRef.current.innerHTML = this.state.product.description;
+    }
+
     render() {
         if (this.state.product === undefined)
             return <div>Loading...</div>;
@@ -25,10 +33,39 @@ export default class SingleProduct extends React.Component {
         if (this.state.product === null)
             return <UnknownPath />;
 
+        const { product, imgIdx } = this.state;
+        const { curCurrencySymbol } = this.context.appState;
+
         return (
-            <div>
-                Hello from Single Product!
+            <div style = {{height: "100vh", display: "flex", justifyContent: "center", alignItems: "center", columnGap: "12px"}}>
+                <div>
+                    <img src = {product.gallery[imgIdx]} 
+                         alt = {`${product.name} number ${imgIdx}`}
+                         style = {{width: "350px", height: "350px"}} />
+                </div>
+                <div>
+                    <h3>
+                        {product.brand}
+                    </h3>
+                    <h4>
+                        {product.name}
+                    </h4>
+                    <div>
+                        <div>
+                            PRICE:
+                        </div>
+                        <div>
+                            {`${curCurrencySymbol} ${product.prices.find(priceObj => priceObj.currency.symbol === curCurrencySymbol).amount}`}
+                        </div>
+                    </div>
+                    <button disabled = {!product.inStock} style = {{border: "1px solid black"}}>
+                        {product.inStock ? "ADD TO CART" : "OUT OF STOCK"}
+                    </button>
+                    <div ref = {this.descriptionRef}></div>
+                </div>
             </div>
         );
     }
 }
+
+DisplayMaxProductInfo.contextType = AppState;
