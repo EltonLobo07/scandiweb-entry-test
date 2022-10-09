68fcbc55ed389698c1f36e312b68c0bfa2446e13
Elton Lobo <eltonsw00@gmail.com>
2022-10-09 12:39:39 +0530

    Add header styles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AppState from "../AppState";
+import CartOverLayOpener from "./CartOverlayOpener";
 import CurrencySwicther from "./CurrencySwitcher";
 import DisplayCartProduct from "./DisplayCartProduct";
 
@@ -12,14 +13,14 @@ export default class Header extends React.Component {
         const cartOverLayDisplay = displayCartOverlay ? "inline-block" : "none";
 
         return (
-            <header style = {{position: "relative", zIndex: "2", border: "1px solid black", display: "flex", justifyContent: "space-around"}}>
+            <header style = {{position: "relative", zIndex: "2", display: "flex", justifyContent: "space-around"}}>
                 <nav style = {{display: "flex", columnGap: "12px"}}>
                     {
                         categoryNames.map(categoryName => {
                             return (
                                 <button key = {categoryName}
                                         onClick = {() => this.handleCategoryNameClick(categoryName)}
-                                        style = {{border: `1px solid ${categoryName === curCategoryName ? "black" : "white"}`}}
+                                        style = {{borderBottomColor: `${categoryName === curCategoryName ? "#5ECE7B" : "white"}`, color: `${categoryName === curCategoryName ? "#5ECE7B" : "black"}`}}
                                 >
                                     {categoryName}
                                 </button>
@@ -28,19 +29,18 @@ export default class Header extends React.Component {
                     }
                 </nav>
 
-                <div>
-                    Shop logo
-                </div>
+                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor">
+                    <path fillRule="evenodd" d="M7.5 6v.75H5.513c-.96 0-1.764.724-1.865 1.679l-1.263 12A1.875 1.875 0 004.25 22.5h15.5a1.875 1.875 0 001.865-2.071l-1.263-12a1.875 1.875 0 00-1.865-1.679H16.5V6a4.5 4.5 0 10-9 0zM12 3a3 3 0 00-3 3v.75h6V6a3 3 0 00-3-3zm-3 8.25a3 3 0 106 0v-.75a.75.75 0 011.5 0v.75a4.5 4.5 0 11-9 0v-.75a.75.75 0 011.5 0v.75z" clipRule="evenodd" />
+                </svg>
 
-                <div style = {{display: "flex", columnGap: "12px", position: "relative"}}>
+                <div className = "mainHeaderContainer">
                     <CurrencySwicther appState = {appState}
                                       setAppState = {setAppState} />
 
-                    <div onClick = {() => this.handleCartClick()}>
-                        Cart ({this.getTotalProductsInTheCart()})
-                    </div>
+                    <CartOverLayOpener onClick = {() => this.handleCartClick()} 
+                                       numItems = {this.getTotalProductsInTheCart()} />
 
-                    <div style = {{position: "absolute", display: cartOverLayDisplay, top: "27px", right: "0px", backgroundColor: "white", padding: "4px", width: "500px"}}>
+                    <div style = {{position: "absolute", display: cartOverLayDisplay, top: "59px", right: "0px", backgroundColor: "white", padding: "4px", width: "500px"}}>
                         <div>
                             My bag {totalQuantity} items
                         </div>
